f05c42497276a89aac5be50032dc0c345a9f6aae
linzowo <linzowo@outlook.com>
2020-05-21 21:59:19 +0800

    feat: 初步完成分类列表页面基础样式

--- pages/categoryList/categoryList.vue
+++ pages/categoryList/categoryList.vue
@@ -2,7 +2,10 @@
 	<view class="category-container flex-direction">
 		
 		<!-- navBar S -->
-		<view class="cu-bar flex-direction mbg-gradual-blue align-stretch nav-bar">
+		<view 
+		class="cu-bar flex-direction mbg-gradual-blue align-stretch nav-bar"
+		:style="{zIndex:pageState.navBtnSelected?0:999}"
+		>
 			
 			<!-- 顶部自定义导航栏 S -->
 			<view class="flex-sub">
@@ -21,7 +24,9 @@
 						{{item}}
 					</view>
 				</scroll-view>
-				<view class="padding nav-bar-btn">
+				<view 
+				@tap="navBtnTap"
+				class="padding nav-bar-btn">
 					<text class="lg text-white cuIcon-unfold"></text>
 				</view>
 			</view>
@@ -57,121 +62,172 @@
 		
 		<!-- content S -->
 		
-		<view class="content-box flex-sub bg-blue">
+		<view class="content-box flex-sub">
 			
-			<uni-popup 
-			ref="filterBarPopup" 
-			:type="'top'"
-			@change="popupChange"
-			:animation="false"
-			class="filter-bar-popup-box"
-			:positionTop="{top: navBarInfo.bottom + 'px'}"
-			:maskClick="true"
-			>
-				<view class="bg-white flex-sub flex-direction">
-					
-					<!-- 弹窗主体 S -->
-					<view 
-					class="filter-list flex-sub solids-top"
-					>
-						
-						<!-- 排序选项 S -->
-						<view
-						v-if="storeNavList[0].selected"
-						 class="cu-list menu flex-sub">
-							<view 
-							v-for="(item,index) in storeNavList[0].list"
-							:key="item.name+index"
-							@tap="orderTap(index)"
-							class="cu-item"
-							:class="storeNavList[0].listSelected ? 
-											(storeNavList[0].listSelectedIndex == index ? 
-													'text-blue':'' ) : ''"
-							>
-								<view class="content">
-									<text class="text-sm">{{item.name}}</text>
-								</view>
-								<view 
-								v-if="storeNavList[0].listSelected && storeNavList[0].listSelectedIndex == index"
-								class="action">
-									<text class="cuIcon-check text-lg"></text>
-								</view>
-							</view>
-						</view>
-						<!-- 排序选项 S -->
-						<!-- 筛选选项 S -->
-						<view 
-						v-else
-						class="flex-sub flex-direction">
-							<view 
-							v-for="(filterValue,name,index) in storeNavList[3].list"
-							:key="index"
-							class="margin-lr-sm flex-direction flex-sub">
-								<text class="text-sm padding-tb-sm">
-									{{['商家服务（可多选）','优惠活动（单选）','人均消费'][index]}}
-								</text>
-								<view 
-								class="cu-list grid col-3 no-border" 
-								:style="{paddingLeft:0,paddingRight:0,paddingTop:0}">
-									<view 
-									v-for="(item,i) in filterValue"
-									:key="i"
-									@tap="filterTap(name,i)"
-									class="cu-item"
-									:style="{padding:0}"
-									>
-										<view 
-										class="margin-top-xs align-center bg-grey-fa margin-right-xs padding-tb-xs justify-center" 
-										:class="[
-											index == 0 ? 
-											(storeNavList[3].selectedIndex[name].includes(i) ? 'cur-item':'') : 
-											(i == storeNavList[3].selectedIndex[name] ? 'cur-item':'')
-											]"
-											>
-											<image 
-											v-if="item.icon_url"
-											class="bar-icon"
-											:src="item.icon_url" mode="aspectFit"></image>
-											{{item.text || item.name || item}}
-										</view>
-									</view>
-								</view>
-							</view>
-							
-							<!-- 按钮 S -->
-							<view class="flex-sub">
-								<text 
-								class="flex-sub bg-white padding solid shadow text-center"
-								:class="
-								storeNavList[3].selectedIndex.filterDataSupports.length ||
-								storeNavList[3].selectedIndex.filterDataActivity != -1 || 
-								storeNavList[3].selectedIndex.averagePrice != -1 ? 
-								'' : 'ban-click'"
-								@tap="storeNavBtnTap('clear')"
-								>清空</text>
-								<text 
-								class="flex-sub bg-green padding solid shadow text-center"
-								@tap="storeNavBtnTap('ok')"
-								>确定</text>
-							</view>
-							<!-- 按钮 E-->
-						</view>
-						
-						<!-- 筛选选项 E -->
-						
-					</view>
-					<!-- 弹窗主体 S -->
-				</view>
+			<!-- 未登录 S -->
+			<view 
+			v-if="!login"
+			class="login-no flex-direction align-center flex-sub justify-center padding-top">
+				<image :src="$i_u.login_no" class="login-img" mode="widthFix"></image>
+				<text class="text-lg text-color-9 margin-bottom">没有结果</text>
+				<text class="text-lg text-color-9">登录后可查看更多商家</text>
+				<navigator url="/pages/login/login" class="link">点此登录 ></navigator>
+			</view>
+			<!-- 未登录 E -->
 			
-			</uni-popup>
+			<!-- 登录 S -->
+			<view
+			 v-if="login"
+			 class="login">
+				登录后
+			</view>
+			<!-- 登录 E -->
 			
 		</view>
 		<!-- content E -->
+		
+		<!-- 弹窗部分 S -->
+		
+		<!-- 筛选及排序弹窗 S -->
+		<uni-popup 
+		ref="filterBarPopup" 
+		:type="'top'"
+		@change="popupChange"
+		:animation="false"
+		class="filter-bar-popup-box"
+		:positionTop="{top: navBarInfo.bottom + 'px'}"
+		>
+			<view class="bg-white flex-sub flex-direction">
+				
+				<!-- 弹窗主体 S -->
+				<view 
+				class="filter-list flex-sub solids-top"
+				>
+					
+					<!-- 排序选项 S -->
+					<view
+					v-if="storeNavList[0].selected"
+					 class="cu-list menu flex-sub order-box">
+						<view 
+						v-for="(item,index) in storeNavList[0].list"
+						:key="item.name+index"
+						@tap="orderTap(index)"
+						class="cu-item"
+						:class="storeNavList[0].listSelected ? 
+										(storeNavList[0].listSelectedIndex == index ? 
+												'text-blue':'' ) : ''"
+						>
+							<view class="content">
+								<text class="text-sm">{{item.name}}</text>
+							</view>
+							<view 
+							v-if="storeNavList[0].listSelected && storeNavList[0].listSelectedIndex == index"
+							class="action">
+								<text class="cuIcon-check text-lg"></text>
+							</view>
+						</view>
+					</view>
+					<!-- 排序选项 S -->
+					
+					<!-- 筛选选项 S -->
+					<view 
+					v-else
+					class="flex-sub flex-direction">
+						<view 
+						v-for="(filterValue,name,index) in storeNavList[3].list"
+						:key="index"
+						class="margin-lr-sm flex-direction flex-sub">
+							<text class="text-sm padding-tb-sm">
+								{{['商家服务（可多选）','优惠活动（单选）','人均消费'][index]}}
+							</text>
+							<view 
+							class="cu-list grid col-3 no-border" 
+							:style="{paddingLeft:0,paddingRight:0,paddingTop:0}">
+								<view 
+								v-for="(item,i) in filterValue"
+								:key="i"
+								@tap="filterTap(name,i)"
+								class="cu-item"
+								:style="{padding:0}"
+								>
+									<view 
+									class="margin-top-xs align-center bg-grey-fa margin-right-xs padding-tb-xs justify-center" 
+									:class="[
+										index == 0 ? 
+										(storeNavList[3].selectedIndex[name].includes(i) ? 'cur-item':'') : 
+										(i == storeNavList[3].selectedIndex[name] ? 'cur-item':'')
+										]"
+										>
+										<image 
+										v-if="item.icon_url"
+										class="bar-icon"
+										:src="item.icon_url" mode="aspectFit"></image>
+										{{item.text || item.name || item}}
+									</view>
+								</view>
+							</view>
+						</view>
+						
+						<!-- 按钮 S -->
+						<view class="flex-sub">
+							<text 
+							class="flex-sub bg-white padding solid shadow text-center"
+							:class="
+							storeNavList[3].selectedIndex.filterDataSupports.length ||
+							storeNavList[3].selectedIndex.filterDataActivity != -1 || 
+							storeNavList[3].selectedIndex.averagePrice != -1 ? 
+							'' : 'ban-click'"
+							@tap="storeNavBtnTap('clear')"
+							>清空</text>
+							<text 
+							class="flex-sub bg-green padding solid shadow text-center"
+							@tap="storeNavBtnTap('ok')"
+							>确定</text>
+						</view>
+						<!-- 按钮 E-->
+					</view>
+					
+					<!-- 筛选选项 E -->
+					
+				</view>
+				<!-- 弹窗主体 S -->
+			</view>
+		
+		</uni-popup>
+		<!-- 筛选及排序弹窗 S -->
+		
+		<!-- nav弹窗 S -->
+		<uni-popup
+		ref="filterCategoryPopup" 
+		:type="'top'"
+		@change="popupChange"
+		:animation="false"
+		class="filter-bar-popup-box"
+		>
+			<view class="category-box">
+				<!-- title S -->
+				<view class="title-box flex-sub bg-grey-fa justify-between">
+					<text class="padding">请选择分类</text>
+					<text 
+					@tap="changePageState({navBtnSelected:false})"
+					class="cuIcon-close text-xl padding"></text>
+				</view>
+				<!-- title E -->
+				
+				<!-- category S -->
+				
+				<!-- category E -->
+			</view>
+		</uni-popup>
+		<!-- nav弹窗 E -->
+		
+		<!-- 弹窗部分 E -->
 		
 	</view>
 </template>
 
 <script>
+	import {mapState,mapMutations} from 'vuex';
 	export default {
 		data() {
 			return {
@@ -179,6 +235,7 @@
 				TabCur: 0,
 				scrollLeft: 0,
 				foodType:[],
+				categoryData:[],
 				// 顶部分类导航栏数据
 				navList: [
 					{
@@ -226,12 +283,19 @@
 					pageState:{
 						login:false, // 登录状态
 						storeNavSelected:false, // 店铺导航栏的nav是否被选中
+						navBtnSelected:false, // 自定义顶部nav按钮被选中
 					}, 
 					// 弹窗栈用于帮助用户关闭多个弹窗
 					popupStack:[],
 		
 			}
 		},
+		computed:{
+			...mapState([
+				'login'
+			])
+		}
+		,
 		watch:{
 			
 			/**
@@ -246,12 +310,29 @@
 				}else{
 					this.closePopup('filterBarPopup');
 				}
+				
+				if(n.navBtnSelected){
+					// 打开筛选弹窗
+					this.openPopup('filterCategoryPopup');
+				}else{
+					this.closePopup('filterCategoryPopup');
+				}
 			}
 		}
 		,
 		created() {
+			// 请求一些渲染页面必须的数据
+			
 			// 模拟网络请求需要数据
 			this.foodType = this.$t_d.FOOD_TYPE_DATA;
+			this.categoryData = this.$t_d.CATEGORE_DATA;
+			
+			
+			// 获取主页单排筛选和排序方式数据
+			this.storeNavList[0].list = this.$t_d.INDEX_SORT_DATA.outside.inside_sort_filter;
+			this.storeNavList[3].list.filterDataSupports = [this.$t_d.INDEX_SORT_DATA.bar.delivery_mode,...this.$t_d.INDEX_SORT_DATA.bar.supports];
+			this.storeNavList[3].list.filterDataActivity = this.$t_d.INDEX_SORT_DATA.bar.activity_types;
+			
 		}
 		,
 		mounted() {
@@ -259,6 +340,11 @@
 		}
 		,
 		methods:{
+			navBtnTap(){
+				// 改变页面状态
+				this.changePageState({navBtnSelected:true});
+			}
+			,
 			/**
 			 * 顶部自定义navbar中的分类点击事件
 			 * @param {Object} e 事件参数对象
@@ -426,6 +512,9 @@
 </script>
 
 <style lang="scss" scoped>
+	page{
+		background-color: #fff;
+	}
 	.content-box{
 		position: relative;
 	}
@@ -438,5 +527,27 @@
 	}
 	.nav-bar-btn{
 		box-shadow: -1px 2px 3px rgba($color: #000000, $alpha: .3);
+	}
+	.login-no{
+		
+	}
+	.link{
+		border-bottom: 1px solid #0081FF;
+		color: #0081FF;
+	}
+	.login-img{
+		width: 400rpx;
+		height: 400rpx;
+	}
+	.bar-icon{
+		width: 24rpx;
+		height: 24rpx;
+		margin-right: 10rpx;
+	}
+	.order-box{
+		width: 750rpx;
+	}
+	.category-box{
+		width: 750rpx;
 	}
 </style>
